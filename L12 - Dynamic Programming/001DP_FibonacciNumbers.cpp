eea5de5ee80d42d342188c577a708b622e4e4194
Abhishek Srivastava <abhishek18124@iiitd.ac.in>
2026-06-29 17:03:14 +0530

    L12 - Dynamic Programming

---
+++ L12 - Dynamic Programming/001DP_FibonacciNumbers.cpp
@@ -0,0 +1,128 @@
+/*
+
+given a non-negative number n, find the nth fibonacci no.
+
+Example :
+	Input : n = 7
+	Output : 13
+
+*/
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int cnt1 = 0;
+
+int f(int n) {
+
+	cnt1++;
+
+	// base case
+
+	if (n == 0 || n == 1) {
+
+		// f(0) : find the 0th fibonacci no.
+
+		// f(1) : find the 1st fibonacci no.
+
+		return n;
+
+	}
+
+	// recursive case
+
+	// f(n) : find the nth fibonacci no.
+
+	return f(n - 1) + f(n - 2);
+}
+
+int cnt2 = 0;
+
+int fTopdown(int n, vector<int>& dp) {
+
+	cnt2++;
+
+	// lookup
+	if (dp[n] != -1) {
+		return dp[n];
+	}
+
+	// base case
+
+	if (n == 0 || n == 1) {
+
+		// f(0) : find the 0th fibonacci no.
+
+		// f(1) : find the 1st fibonacci no.
+
+		return dp[n] = n;
+
+	}
+
+	// recursive case
+
+	// f(n) : find the nth fibonacci no.
+
+	return dp[n] = fTopdown(n - 1, dp) + fTopdown(n - 2, dp);
+}
+
+// time : O(n)
+// space: O(n) due to dp[]
+
+int fBottomUp(int n) {
+
+	vector<int> dp(n + 1);
+
+	dp[0] = 0; // at the 0th index of dp[] we store f(0)
+	dp[1] = 1; // at the 1st index of dp[] we store f(1)
+
+	for (int i = 2; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+
+	return dp[n]; // at the nth index of dp[] we store f(n)
+
+}
+
+// time : O(n)
+// space: O(1)
+
+int fBottomUpSpaceOptimised(int n) {
+
+	int a = 0; // at the 0th index of dp[] we store f(0)
+	int b = 1; // at the 1st index of dp[] we store f(1)
+	int c;
+
+	for (int i = 2; i <= n; i++) {
+		c = a + b;
+		a = b;
+		b = c;
+	}
+
+	return c; // at the nth index of dp[] we store f(n)
+
+}
+
+
+
+int main() {
+
+	int n = 20;
+
+	cout << f(n) << endl;
+
+	vector<int> dp(n + 1, -1);
+
+	cout << fTopdown(n, dp) << endl;
+
+	// cout << cnt1 << endl;
+	// cout << cnt2 << endl;
+
+	cout << fBottomUp(n) << endl;
+
+	cout << fBottomUpSpaceOptimised(n) << endl;
+
+	return 0;
+}
